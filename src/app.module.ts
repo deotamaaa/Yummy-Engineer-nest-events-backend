83e9538a02e54b0429bf4c0428e0484c54ac8c89
Deotama <deotama11@gmail.com>
2022-03-10 13:28:06 +0700

    Video 4.9 Done.

--- src/app.module.ts
+++ src/app.module.ts
@@ -6,7 +6,8 @@ import { Event } from './event.entity';
 import { EventsController } from './events.controller';
 
 @Module({
-  imports: [TypeOrmModule.forRoot({
+  imports: [
+    TypeOrmModule.forRoot({
     type: 'mysql',
     host: '127.0.0.1',
     port: 3306,
@@ -15,7 +16,9 @@ import { EventsController } from './events.controller';
     database: 'nest-events',
     entities: [Event],
     synchronize: true,
-  })],
+  }),
+  TypeOrmModule.forFeature([Event]),
+],
   controllers: [
     AppController,
     EventsController],
