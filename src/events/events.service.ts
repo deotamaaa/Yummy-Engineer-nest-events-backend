ab8d0119de1714e1d778c3fcc40beaf1070dce1d
Deotama <deotama11@gmail.com>
2022-03-14 16:14:14 +0700

    Video 8.9 Done.

--- src/events/events.service.ts
+++ src/events/events.service.ts
@@ -1,5 +1,6 @@
 import { Injectable, Logger } from "@nestjs/common";
 import { InjectRepository } from "@nestjs/typeorm";
+import { paginate, PaginateOptions } from "src/pagination/paginator";
 import { Repository } from "typeorm";
 import { AttendeeAnswerEnum } from "./attendee.entity";
 import { Event } from "./event.entity";
@@ -51,11 +52,13 @@ export class EventsService {
                             answer: AttendeeAnswerEnum.Rejected
                         }))
     }
-    public async getEventsWithAttendeeCountFiltered(filter?: ListEvents) {
+    private async getEventsWithAttendeeCountFiltered(
+        filter?: ListEvents
+    ) {
         let query = this.getEventsWithAttendeeCountQuery();
 
         if (!filter) {
-            return query.getMany();
+            return query;
         }
 
         if (filter.when) {
@@ -80,7 +83,17 @@ export class EventsService {
             }
         }
 
-        return await query.getMany();
+        return query;
+    }
+
+    public async getEventsWithAttendeeCountFilteredPaginated(
+        filter: ListEvents,
+        paginateOptions: PaginateOptions
+    ) {
+        return await paginate(
+            await this.getEventsWithAttendeeCountFiltered(filter),
+            paginateOptions
+        );
     }
 
     public async getEvent(id: number): Promise<Event | undefined> {
